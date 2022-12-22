9a36cfccd7b98b8ab758040b8d37390835da6e83
CRV <98711930+izCRV@users.noreply.github.com>
2022-12-22 15:14:09 +0100

    Add files via upload

---
+++ includes/minitalk.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aizsak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 08:23:59 by aizsak            #+#    #+#             */
+/*   Updated: 2022/12/22 15:02:25 by aizsak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+
+# include "../libft/libft.h"
+# include <signal.h>
+
+# include <sys/types.h>
+# include <string.h>
+
+typedef struct s_bin
+{
+	int	i;
+	int	y;
+}	t_bin;
+
+#endif
---
+++ src/client.c
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aizsak <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/19 08:19:00 by aizsak            #+#    #+#             */
+/*   Updated: 2022/12/22 15:09:23 by aizsak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int	g_global;
+
+static void	str_to_binary(char *s, pid_t pid, t_bin bin)
+{
+	bin.i = 0;
+	while (bin.i <= (int)ft_strlen(s))
+	{
+		bin.y = 7;
+		while (bin.y >= 0)
+		{
+			g_global = 0;
+			if (s[bin.i] & 1 << bin.y)
+			{
+				if (kill(pid, SIGUSR1) == -1)
+					exit(-1);
+			}
+			else
+			{
+				if (kill(pid, SIGUSR2) == -1)
+					exit(-1);
+			}
+			while (g_global == 0)
+				;
+			bin.y--;
+		}
+		bin.i++;
+	}
+}
+
+static void	signal_checker(int signum, siginfo_t *siginfo, void *ucontext)
+{
+	(void)ucontext;
+	if (signum != SIGUSR1)
+		exit(-1);
+	if (siginfo->si_code != SI_USER)
+		exit(-1);
+}
+
+static void	signal_handler(int signum, siginfo_t *siginfo, void *ucontext)
+{
+	(void)ucontext;
+	signal_checker(signum, siginfo, ucontext);
+	if (signum == SIGUSR1)
+		g_global = 1;
+}
+
+int	main(int argc, char **argv)
+{
+	pid_t				pid;
+	t_bin				bin;
+	struct sigaction	signal_action;
+
+	pid = ft_atoi(argv[1]);
+	if (argc < 3 || pid < 0)
+	{
+		ft_printf("Valid form: ./client <pid_server><message>\n");
+		exit(-1);
+	}
+	signal_action.sa_sigaction = signal_handler;
+	sigemptyset(&signal_action.sa_mask);
+	signal_action.sa_flags = SA_SIGINFO;
+	if (sigaction(SIGUSR1, &signal_action, NULL) < 0)
+		exit(-1);
+	str_to_binary(argv[2], pid, bin);
+	exit(0);
+}
