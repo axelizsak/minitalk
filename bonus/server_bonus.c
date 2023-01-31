04120e19c8f291c65b9ac28326621013b5ae98e9
CRV <98711930+izCRV@users.noreply.github.com>
2023-01-31 09:12:33 +0100

    Add files via upload

--- bonus/server_bonus.c
+++ bonus/server_bonus.c
@@ -6,7 +6,7 @@
 /*   By: aizsak <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 15:27:43 by aizsak            #+#    #+#             */
-/*   Updated: 2022/12/22 15:36:16 by aizsak           ###   ########.fr       */
+/*   Updated: 2023/01/31 09:10:03 by aizsak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,7 +38,10 @@ static void	signal_handler(int signum, siginfo_t *siginfo, void *ucontext)
 		if (c != '\0')
 			write (1, &c, 1);
 		else if (c == '\0')
+		{
 			write (1, "\n", 1);
+			//kill(siginfo->si_pid, SIGUSR2);
+		}
 		i = 0;
 		c = 0;
 	}
